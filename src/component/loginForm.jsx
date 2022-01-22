2419c69ba29625eeb573bee5bb945a0eef5b44cf
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-01-22 20:27:42 +0700

    add onChange function

--- src/component/loginForm.jsx
+++ src/component/loginForm.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 
-const InputForm = ({label, type, name}) => {
+const InputForm = ({label, type, name, onChange}) => {
     return (
         <div className="my-3 row">
             <label className="form-label" htmlFor="email">{label}:</label>
-            <input type={type} name={name} />
+            <input type={type} name={name} onChange={e => onChange(e.target.value)} />
         </div>
     )
 }
 
 export default class LoginForm extends React.Component {
+
+    state = {
+        email: "",
+        password: "",
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
     render() {
         return (
-            <form className="container d-flex flex-column">
-                <InputForm label={"Email"} type={"email"} name={"email"} />
-                <InputForm label={"Password"} type={"password"} name={"password"} />
-                <div className="my-3">
-                    <input type="checkbox" name="member" className="form-check-input"/>
-                    <label className="form-check-label ms-2" htmlFor="member">Member:</label>
-                </div>
+            <form className="container d-flex flex-column" onSubmit={this.handleSubmit}>
+                <InputForm label={"Email"} type={"email"} name={"email"}
+                    onChange={value => this.setState({email: value})} />
+                <InputForm label={"Password"} type={"password"} name={"password"}
+                    onChange={value => this.setState({ password: value })} />
                 <button type="submit" className="btn btn-success m-3 align-self-center">Submit</button>
             </form>
         );
