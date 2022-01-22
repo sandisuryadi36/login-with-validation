aeab71e667dc7a6a36d4497510eb299bcc73c90d
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-01-22 19:59:01 +0700

    update view

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import LoginForm from "./component/loginForm";
 
 function App() {
   return (
-    <div className="card col-3 center">
+    <div className="card col-lg-3 center">
       <div className="card-header text-center">
         Login Your Account
       </div>
--- src/component/loginForm.jsx
+++ src/component/loginForm.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
+const InputForm = ({label, type, name}) => {
+    return (
+        <div className="my-3 row">
+            <label className="form-label" htmlFor="email">{label}:</label>
+            <input type={type} name={name} />
+        </div>
+    )
+}
+
 export default class LoginForm extends React.Component {
     render() {
         return (
             <form className="container d-flex flex-column">
-                <div className="my-3 d-flex flex-column">
-                    <label className="form-label" htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="emailInput" />
-                </div>
-                <div className="my-3 d-flex flex-column">
-                    <label className="form-label" htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="passwordlInput" />
+                <InputForm label={"Email"} type={"email"} name={"email"} />
+                <InputForm label={"Password"} type={"password"} name={"password"} />
+                <div className="my-3">
+                    <input type="checkbox" name="member" className="form-check-input"/>
+                    <label className="form-check-label ms-2" htmlFor="member">Member:</label>
                 </div>
                 <button type="submit" className="btn btn-success m-3 align-self-center">Submit</button>
             </form>
